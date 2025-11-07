2d0928d17c4fe303496a598409e35eece6f81e43
pradipta <pradiptasundarnayak5@gmail.com>
2025-11-07 09:08:07 +0530

    make changes in main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "my_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Outbound rules
@@ -39,7 +39,7 @@ resource "aws_security_group" "my_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
